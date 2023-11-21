3a21e23c08d68cd6730d813cb482890d34d538c7
Tyler Riley <tylerjriley@gmail.com>
2023-11-20 21:25:44 -0500

    Created ListUtil and refactor of InventoryManager

---
+++ PerkUp/ListUtil.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace PerkUp;
+
+public class ListUtil
+{
+
+
+    public static bool CheckListForItem<T>(T refranceItem, List<T> listRefrance)
+    {
+        if (refranceItem != null)
+        {
+            if (listRefrance.Contains(refranceItem))
+            {
+                return true;
+            }
+            else 
+            {
+                return false;
+            }
+        }
+        else if (listRefrance.Count != 0)
+        {
+            return false;
+        }
+        else 
+        {
+            Console.WriteLine("No List Avalable. Retruning False");
+            return false;
+
+        }
+    }
+}
---
+++ PerkUp/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+
+/*Program CeckList
+    Inventory Manager
+    Employee Manager
+    Event Scheduler
+    Day Record Tracking ie: Sales Numbers, Customer Tickets, Ticket Times, Menu Item Popularity....
+    Notifications ie : Low Inventory, Event Reminders, Menu Update ....
+    
+    DataBase Intergration
+*/
+ 
+
+namespace PerkUp
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            
+            InventoryManager inventoryManager = new InventoryManager();
+            bool runningProgram = true;
+        while(runningProgram)
+        {
+            inventoryManager.AddItem(inventoryManager.InventoryItemBuilder());
+
+            inventoryManager.ViewAllList();
+    
+            inventoryManager.InspectItemByName(inventoryManager.InputStringToItem());
+
+            inventoryManager.ViewAllList();
+
+
+
+        
+            Console.Write("Would you like to continue? (y) yes (n)no: ");
+            string userInput = Console.ReadLine().ToLower();
+            if(userInput == "n" || userInput == "no")
+            {
+                runningProgram = false;
+            }
+        }
+           
+
+
+        }
+    }
+}
